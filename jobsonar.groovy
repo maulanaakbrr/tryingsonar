99b84b56d51c01eec8c0a31107a9af4963d422fb
Maulana Akbar Renaldi <maulanaakbar.renaldi@gmail.com>
2024-10-17 13:58:29 +0700

    menambahkan jobsonar jenkins job

---
+++ jobsonar.groovy
@@ -0,0 +1,33 @@
+// Membuat job pipeline pertama (Dev1)
+pipelineJob('NPM-Testing-Dev1') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/maulanaakbrr/nodejs-testing.git')  // URL repo
+                    }
+                    branch('dev1')  // Branch yang digunakan
+                }
+            }
+            scriptPath('Jenkinsfile')  // Lokasi Jenkinsfile di repo
+        }
+    }
+}
+
+// Membuat job pipeline kedua (Dev2)
+pipelineJob('NPM-Testing-Dev2') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/maulanaakbrr/nodejs-testing.git')  // URL repo
+                    }
+                    branch('dev2')  // Branch yang digunakan
+                }
+            }
+            scriptPath('Jenkinsfile')  // Lokasi Jenkinsfile di repo
+        }
+    }
+}
