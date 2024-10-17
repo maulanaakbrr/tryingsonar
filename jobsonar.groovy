e4326b043d8c407ccab8818575e67c6ef3b47768
Maulana Akbar Renaldi <maulanaakbar.renaldi@gmail.com>
2024-10-17 14:48:25 +0700

    perubahan pada repo

--- jobsonar.groovy
+++ jobsonar.groovy
@@ -1,33 +1,39 @@
-// Update pipeline untuk menggunakan branch yang benar (misalnya 'main')
+// Pipeline untuk NPM-Testing-Dev1
 pipelineJob('NPM-Testing-Dev1') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // URL repo yang benar
+                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // Repository yang benar
                     }
-                    branch('main')  // Pastikan ini menggunakan branch yang ada
+                    branch('main')  // Branch yang dipantau, misalnya 'main'
                 }
             }
-            scriptPath('Jenkinsfile')
+            scriptPath('Jenkinsfile')  // Path ke Jenkinsfile di dalam repo
         }
+    }
+    triggers {
+        pollSCM('H/5 * * * *')  // Konfigurasi polling setiap 5 menit
     }
 }
 
-// Pipeline kedua juga diupdate ke branch yang benar
+// Pipeline untuk NPM-Testing-Dev2
 pipelineJob('NPM-Testing-Dev2') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // URL repo yang benar
+                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // Repository yang sama
                     }
-                    branch('main')  // Sesuaikan dengan branch yang benar
+                    branch('main')  // Branch yang dipantau
                 }
             }
-            scriptPath('Jenkinsfile')
+            scriptPath('Jenkinsfile')  // Path ke Jenkinsfile di dalam repo
         }
+    }
+    triggers {
+        pollSCM('H/5 * * * *')  // Konfigurasi polling setiap 5 menit
     }
 }
