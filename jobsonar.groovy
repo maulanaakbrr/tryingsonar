442c439686b0efd8d901ed7e43b63da165b7aa06
Maulana Akbar Renaldi <maulanaakbar.renaldi@gmail.com>
2024-10-22 11:18:46 +0700

    fixing bugs code

--- jobsonar.groovy
+++ jobsonar.groovy
@@ -1,39 +1,53 @@
-// Pipeline untuk NPM-Testing-Dev1
-pipelineJob('NPM-Testing-Development-1') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // Repository yang benar
-                    }
-                    branch('main')  // Branch yang dipantau
-                }
-            }
-            scriptPath('Jenkinsfile')  // Path ke Jenkinsfile di repo
-        }
-    }
-    triggers {
-        scm('H/5 * * * *')  // Perbaikan triggers menggunakan 'scm' alih-alih 'pollSCM'
-    }
-}
+pipeline {
+    agent any
 
-// Pipeline untuk NPM-Testing-Dev2
-pipelineJob('NPM-Testing-Development-2') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/maulanaakbrr/tryingsonar.git')  // Repository yang benar
+    tools {
+        nodejs "NodeJS" // Sesuaikan dengan NodeJS yang sudah diinstal di Jenkins
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/maulanaakbrr/tryingsonar.git'
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                sh 'npm test'
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    def scannerHome = tool 'SonarQube Scanner' // Pastikan SonarQube Scanner sudah diinstal di Jenkins
+                    withSonarQubeEnv('Sonarqube VM') { // Gunakan nama konfigurasi SonarQube yang sudah kamu buat
+                        sh """
+                        ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=my-project-key \
+                        -Dsonar.sources=. \
+                        -Dsonar.host.url=http://localhost:9000 \
+                        -Dsonar.login=${SONAR_AUTH_TOKEN}
+                        """
                     }
-                    branch('main')  // Branch yang dipantau
                 }
             }
-            scriptPath('Jenkinsfile')  // Path ke Jenkinsfile di repo
         }
     }
-    triggers {
-        scm('H/5 * * * *')  // Perbaikan triggers menggunakan 'scm' alih-alih 'pollSCM'
+
+    post {
+        success {
+            echo 'Tes berhasil dijalankan!'
+        }
+        failure {
+            echo 'Tes gagal dijalankan.'
+        }
     }
 }
